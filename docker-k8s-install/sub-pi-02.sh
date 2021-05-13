a4058b0b051ab04574c87695e29d696cc9fabc5d
Aliya <22922673+aliyacodes@users.noreply.github.com>
2021-05-13 11:42:39 -0700

    Create sub-pi-02.sh
    Automating Docker and Kubernetes basic install and setup for Raspberry Pi 4 cluster/homelab.  Work in progress.

---
+++ docker-k8s-install/sub-pi-02.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+# sub-pi-02 (worker node)
+sudo apt update && sudo apt dist-upgrade -y
+
+chmod 700 .ssh
+chmod 600 .ssh/authorized_keys
+
+sudo adduser eros
+sudo usermod -aG sudo eros
+
+# Disable password based login
+sed -i 's/PasswordAuthentication yes/PasswordAuthentication no/g' /etc/ssh/sshd_config
+
+# Change hostname
+sed -i 's/ubuntu/sub-pi-02/g' /etc/hostname
+
+# Add host list to each machine
+sed -i '2 i 192.168.0.10 dom-pi \
+192.168.0.11 sub-pi-01 \
+192.168.0.12 sub-pi-02 \
+192.168.0.13 sub-pi-03' /etc/hosts
+
+# Enable cgroups
+sed 's/$/ cgroup_enable=cpuset cgroup_memory=1 cgroup_enable=memory/' /boot/firmware/cmdline.txt
+
+# ??? sudo systemctl reload ssh
+
+sudo apt update && sudo apt dist-upgrade -y
+
+# Docker
+sudo apt-get remove docker docker-engine docker.io containerd runc
+
+curl -sSL get.docker.com | sh
+
+sudo groupadd docker
+sudo usermod -aG docker $USER
+newgrp docker
+
+echo '{
+      "exec-opts": ["native.cgroupdriver=systemd"],
+      "log-driver": "json-file",
+      "log-opts": {
+        "max-size": "100m"
+      },
+      "storage-driver": "overlay2"
+      }' >> /etc/docker/daemon.json
+
+
+sed -i 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/g' /etc/sysctl.conf
+
+sudo systemctl restart sshd
+
+echo 'Checking Docker Status...'
+systemctl status docker
+
+echo 'Testing Docker "Hello-World"...'
+docker run hello-world
+
+# Kubernetes
+echo 'deb http://apt.kubernetes.io/ kubernetes-xenial main' >> /etc/apt/sources.list.d/kubernetes.list
+
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+sudo apt update
+
+sudo apt install kubeadm kubectl kubelet -y
