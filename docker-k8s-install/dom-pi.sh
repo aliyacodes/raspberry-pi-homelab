6657ea4f53f38c7413967828c6286e569cde60da
Aliya <22922673+aliyacodes@users.noreply.github.com>
2021-05-23 20:42:36 -0700

    Update dom-pi.sh

--- docker-k8s-install/dom-pi.sh
+++ docker-k8s-install/dom-pi.sh
@@ -2,26 +2,26 @@
 # dom-pi (master node)
 sudo apt update && sudo apt dist-upgrade -y
 
-chmod 700 .ssh
-chmod 600 .ssh/authorized_keys
+sudo chmod 700 .ssh
+sudo chmod 600 .ssh/authorized_keys
 
 sudo adduser eros
 sudo usermod -aG sudo eros
 
 # Disable password based login
-sed -i 's/PasswordAuthentication yes/PasswordAuthentication no/g' /etc/ssh/sshd_config
+sudo sed -i 's/PasswordAuthentication yes/PasswordAuthentication no/g' /etc/ssh/sshd_config
 
 # Change hostname
-sed -i 's/ubuntu/dom-pi/g' /etc/hostname
+sudo sed -i 's/ubuntu/dom-pi/g' /etc/hostname
 
 # Add host list to each machine
-sed -i '2 i 192.168.0.10 dom-pi \
+sudo sed -i '2 i 192.168.0.10 dom-pi \
 192.168.0.11 sub-pi-01 \
 192.168.0.12 sub-pi-02 \
 192.168.0.13 sub-pi-03' /etc/hosts
 
 # Enable cgroups
-sed -i 's/$/ cgroup_enable=cpuset cgroup_memory=1 cgroup_enable=memory/' /boot/firmware/cmdline.txt
+sudo sed -i 's/$/ cgroup_enable=cpuset cgroup_memory=1 cgroup_enable=memory/' /boot/firmware/cmdline.txt
 
 # ??? sudo systemctl reload ssh
 
@@ -30,13 +30,13 @@ sudo apt update && sudo apt dist-upgrade -y
 # Docker
 sudo apt-get remove docker docker-engine docker.io containerd runc
 
-curl -sSL get.docker.com | sh
+sudo curl -sSL get.docker.com | sh
 
 sudo groupadd docker
 sudo usermod -aG docker $USER
-newgrp docker
+sudo newgrp docker
 
-echo '{
+sudo echo '{
       "exec-opts": ["native.cgroupdriver=systemd"],
       "log-driver": "json-file",
       "log-opts": {
@@ -46,7 +46,7 @@ echo '{
       }' >> /etc/docker/daemon.json
 
 
-sed -i 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/g' /etc/sysctl.conf
+sudo sed -i 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/g' /etc/sysctl.conf
 
 # ??? sudo systemctl restart sshd
 
@@ -67,7 +67,7 @@ sudo apt install kubeadm kubectl kubelet -y
 # Master/dom-pi only
 sudo kubeadm init --pod-network-cidr=10.244.0.0/16
 
-mkdir -p $HOME/.kube
+sudo mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
