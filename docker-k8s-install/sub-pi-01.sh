dc289fdb77521931ca9fbb3f4d10d2078454a218
Aliya <22922673+aliyacodes@users.noreply.github.com>
2021-05-24 15:02:55 -0700

    Update sub-pi-01.sh

--- docker-k8s-install/sub-pi-01.sh
+++ docker-k8s-install/sub-pi-01.sh
@@ -60,8 +60,9 @@ docker run hello-world
 
 # Kubernetes
 sudo touch /etc/apt/sources.list.d/kubernetes.list
-
 sudo echo 'deb http://apt.kubernetes.io/ kubernetes-xenial main' > /etc/apt/sources.list.d/kubernetes.list
+
+sudo apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
 
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 sudo apt update
